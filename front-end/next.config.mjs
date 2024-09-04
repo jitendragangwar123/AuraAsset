3cca3e34fa1085103c09154ce62da1bf1e8a5a2d
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-09-04 23:09:01 +0530

    fix:cloudinary image url configuration

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
     webpack: config => {
         config.externals.push('pino-pretty', 'lokijs', 'encoding')
         return config
-    }
+    },
+    images: {
+        domains: ['res.cloudinary.com'],
+    }, 
 }
 
 export default nextConfig;
