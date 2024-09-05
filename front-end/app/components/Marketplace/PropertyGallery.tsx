7908902cf8483167b96a7ed3160734bb6900490f
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2024-09-05 23:38:53 +0530

    feat:sign protocol integration for asset data attestation

--- front-end/app/components/Marketplace/PropertyGallery.tsx
+++ front-end/app/components/Marketplace/PropertyGallery.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Select, { SingleValue, StylesConfig } from "react-select";
 import { FaMapMarkerAlt, FaDollarSign } from "react-icons/fa";
 import { useRouter } from "next/navigation";
+import { SignProtocolClient, SpMode, EvmChains } from "@ethsign/sp-sdk";
+import { privateKeyToAccount } from "viem/accounts";
+import { useAccount } from "wagmi";
+import toast from "react-hot-toast";
 
-type OptionType = {
-  value: string;
-  label: string;
-};
+const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY;
+const ownerAddress = privateKeyToAccount(`0x${privateKey}`);
 
 type Location = {
   address: string;
@@ -26,6 +27,7 @@ type PropertyCardProps = {
   no_of_tokens: string;
   apy: string;
   status: string;
+  property_type: string;
 };
 
 const PropertyCard: React.FC<PropertyCardProps> = ({
@@ -38,11 +40,106 @@ const PropertyCard: React.FC<PropertyCardProps> = ({
   no_of_tokens,
   apy,
   status,
+  property_type,
 }) => {
   const router = useRouter();
-  const handleViewDetails = (id: string) => {
+  const { address } = useAccount();
+  const [attestationCompleted, setAttestationCompleted] = useState(false);
+  const [schemaID, setSchemaID] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSchemaAndAttestation = async (
+    assetType: string,
+    assetDetails: string[],
+    issuer: string,
+    totalSupply: number
+  ) => {
+    try {
+      setLoading(true);
+      const client = new SignProtocolClient(SpMode.OnChain, {
+        chain: EvmChains.sepolia,
+        account: ownerAddress,
+      });
+      toast.loading("Wait for Schema Creation........!");
+      const schemaResponse = await client.createSchema({
+        name: "Aura Asset",
+        data: [
+          { name: "assetType", type: "string" },
+          { name: "assetDetails", type: "string[]" },
+          { name: "issuer", type: "address" },
+          { name: "totalSupply", type: "uint256" },
+        ],
+      });
+
+      console.log("Schema created:", schemaResponse.schemaId);
+      setSchemaID(schemaResponse.schemaId);
+      toast.dismiss();
+      toast.success("Schema Created!");
+      toast.loading("Wait for Attestation........!");
+      if (schemaResponse) {
+        const attestationResponse = await client.createAttestation({
+          schemaId: schemaResponse.schemaId,
+          attestTimestamp: 0,
+          revokeTimestamp: 0,
+          validUntil: 0,
+          dataLocation: 0,
+          revoked: false,
+          recipients: [issuer],
+          data: {
+            assetType,
+            assetDetails,
+            issuer,
+            totalSupply,
+          },
+          indexingValue: issuer.toLowerCase(),
+        });
+        console.log("Attestation Done: ", attestationResponse);
+        toast.dismiss();
+        toast.success(`Attestation Done!`);
+        setAttestationCompleted(true);
+        queryAttestations();
+      }
+    } catch (error) {
+      console.error("Error creating schema or attestation:", error);
+      toast.error("Error creating schema or attestation!");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  async function makeAttestationRequest(endpoint: string, options: any) {
+    const url = `https://testnet-rpc.sign.global/api/${endpoint}`;
+
+    const response = await fetch(url, {
+      method: options.method || "GET",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        ...options.headers,
+      },
+      body: options.body ? JSON.stringify(options.body) : null,
+    });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(JSON.stringify(errorData));
+    }
+    return await response.json();
+  }
+
+  async function queryAttestations() {
+    const response = await makeAttestationRequest("index/attestations", {
+      method: "GET",
+      params: {
+        mode: "onchain",
+        schemaId: schemaID,
+        attester: address,
+      },
+    });
+    console.log(response.data.rows);
+  }
+
+  const handleViewDetails = (title: string) => {
     if (status !== "sold out") {
-      router.push(`/market-place/${encodeURIComponent(id)}`);
+      router.push(`/market-place/${encodeURIComponent(title)}`);
     }
   };
 
@@ -87,17 +184,48 @@ const PropertyCard: React.FC<PropertyCardProps> = ({
             <span className="font-bold text-xl">{apy}%</span>
           </div>
         </div>
-        <button
-          onClick={() => handleViewDetails(title)}
-          className={`w-full ${
-            status === "sold out"
-              ? "bg-gray-400"
-              : "bg-blue-500 hover:bg-blue-700"
-          } text-white font-bold py-2 px-4 rounded transition duration-300`}
-          disabled={status === "sold out"}
-        >
-          {status === "sold out" ? "Sold Out" : "View Details"}
-        </button>
+        <div className="flex justify-between mt-4 space-x-2">
+          <button
+            onClick={() => handleViewDetails(title)}
+            className={`w-1/2 ${
+              !attestationCompleted
+                ? "bg-gray-400"
+                : "bg-blue-500 hover:bg-blue-700"
+            } text-white font-bold py-2 px-4 rounded transition duration-300`}
+            disabled={!attestationCompleted}
+          >
+            {attestationCompleted ? "View Details" : "View Details"}
+          </button>
+          <button
+            onClick={() =>
+              handleSchemaAndAttestation(
+                property_type,
+                [
+                  title,
+                  desc,
+                  location.address,
+                  location.city,
+                  location.state,
+                  location.country,
+                ],
+                "0xB04Ba78b7413C3265ffC1293a4659dEA54Ef0851",
+                Number(no_of_tokens)
+              )
+            }
+            className={`w-1/2 ${
+              status === "sold out" || attestationCompleted || loading
+                ? "bg-gray-400"
+                : "bg-blue-500 hover:bg-blue-700"
+            } text-white font-bold py-2 px-4 rounded transition duration-300`}
+            disabled={status === "sold out" || attestationCompleted || loading}
+          >
+            {status === "sold out"
+              ? "View Attestation"
+              : loading
+              ? "Processing..."
+              : "Make Attestation"}
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -113,6 +241,7 @@ const PropertyGallery: React.FC = () => {
         const response = await fetch(
           "https://aura-asset-back-end.vercel.app/get-property-details"
         );
+        if (!response.ok) throw new Error("Failed to fetch property data");
         const data = await response.json();
         setProperties(data);
       } catch (error) {
@@ -128,9 +257,7 @@ const PropertyGallery: React.FC = () => {
   };
 
   const filteredProperties = properties.filter((property) => {
-    return (
-      (!selectedRadio || property.status === selectedRadio)
-    );
+    return !selectedRadio || property.status === selectedRadio;
   });
 
   return (
@@ -175,18 +302,7 @@ const PropertyGallery: React.FC = () => {
       </div>
       <div className="flex flex-wrap gap-2 p-4">
         {filteredProperties.map((property, index) => (
-          <PropertyCard
-            key={index}
-            title={property.title}
-            desc={property.desc}
-            total_price={property.total_price}
-            images={property.images}
-            location={property.location}
-            token_name={property.token_name}
-            no_of_tokens={property.no_of_tokens}
-            apy={property.apy}
-            status={property.status}
-          />
+          <PropertyCard key={index} {...property} />
         ))}
       </div>
     </div>
